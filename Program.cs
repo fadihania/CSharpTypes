0fddc178d327e60737ffaf1bd8b72f60486b2f15
Fadi Hania <fhania@gmail.com>
2023-03-05 10:25:44 +0200

    Difference between Value & Reference types

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 ﻿using CSTypes;
 
-Console.WriteLine(User.Seed);
+// Value type
+int x = 5;
+int y = x;
+Console.WriteLine(x); // => 5
+Console.WriteLine(y); // => 5
+
+x = 10;
+Console.WriteLine(x); // => 10
+Console.WriteLine(y); // => 5
 
 
+// Reference type
 var u1 = new User();
-User u2;
-
-// Private member, cannot access
-//u1.id = "kf938k237sl";
-
-//u1.SetName("Ahmad");
-//Console.WriteLine(u1.GetName());
+User u2 = u1;
 
 u1.Name = "Ahmad";
-Console.WriteLine(u1.Name);
+Console.WriteLine(u1.Name); // => Ahmad
+Console.WriteLine(u2.Name); // => Ahmad
+
+u1.Name = "Majed";
+Console.WriteLine(u1.Name); // => Majed
+Console.WriteLine(u2.Name); // => Majed
 
 u1.Email = "ahmad@example.com";
 
